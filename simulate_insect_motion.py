44d5e5f9ea6af5663643a8b57080180c358dd3f0
Jonathan Lu <jonylu@gmail.com>
2019-06-30 10:49:50 -0700

    mouse click and arrow keys for ant motions

--- simulate_insect_motion.py
+++ simulate_insect_motion.py
@@ -5,81 +5,105 @@ Created on Fri Jan 18 21:34:19 2019
 @author: jonyl
 """
 import numpy as np
-from antworldsimulator import Ant
-from antworldsimulator import AntWorld
 import pygame
+
+DIRT = 0
+AIR = 1
+ANT = 2
+social_security = 0
+
 
 def advance_simulator():
     print ("simulation advanced")
-    
 
-class Simulator:
-    def __init__(self, start_loc = np.array([0, 0]), final_loc = np.array([0, 0])):
+class Ant:
+    def __init__(self,  start_pos = np.array([0, 0]), ant_type = 'worker'):
+        self.current_pos = start_pos
         self.is_blue = True
-        self.current_loc = start_loc
-        self.final_loc = final_loc
-    def return_current_pos(self):
-        return self.current_loc
-    def return_final_pos(self):
-        return self.final_loc
-    def return_x_current(self):
-        return self.current_loc[0]
-    def return_y_current(self):
-        return self.current_loc[1]
-    def return_x_final(self):
-        return self.final_loc[0]
-    def return_y_final(self):
-        return self.final_loc[1]
+        self.current_pos = start_pos
+        self.final_pos = start_pos
+        self.ant_type = ant_type
+        self.is_selected = False
+        global social_security
+        self.ant_social_security = social_security
+        social_security = social_security + 1
+        
+    def __str__(self):
+        return ('(' + str(self.current_pos[0]) + ',' + str(self.current_pos[1]) + ')' + \
+            ' ' + str(self.ant_social_security))
     
-    def increment_x_current(self, incr):
-        self.current_loc[0] += incr
-    def increment_y_current(self, incr):
-        self.current_loc[1] += incr
+    def return_key(self):
+        return (str(self.current_pos))
+    
+    def return_x_current_pos(self):
+        return self.current_pos[0]
+    
+    def return_y_current_pos(self):
+        return self.current_pos[1]
+    
+    def return_x_final_pos(self):
+        return self.final_pos[0]
+    
+    def return_y_final_pos(self):
+        return self.final_pos[1]
+
+    def increment_x_current_pos(self, incr):
+        self.current_pos[0] += incr
+        
+    def increment_y_current_pos(self, incr):
+        self.current_pos[1] += incr
     
     def advance_simple(self):
-        if self.return_x_current() < self.return_x_final():
-            self.current_loc[0] += 1
-        if self.return_x_current() > self.return_x_final():
-            self.current_loc[0] -= 1
-        if self.return_y_current() < self.return_y_final():
-            self.current_loc[1] += 1
-        if self.return_y_current() > self.return_y_final():
-            self.current_loc[1] -= 1   
-    def set_destination(self, final_loc):
-        self.final_loc = final_loc
+        if self.return_x_current_pos() < self.return_x_final_pos():
+            self.current_pos[0] += 1
+        if self.return_x_current_pos() > self.return_x_final_pos():
+            self.current_pos[0] -= 1
+        if self.return_y_current_pos() < self.return_y_final_pos():
+            self.current_pos[1] += 1
+        if self.return_y_current_pos() > self.return_y_final_pos():
+            self.current_pos[1] -= 1   
+            
+    def set_destination(self, final_pos):
+        self.final_pos = final_pos
         
     def toggle_color(self):
         self.is_blue = not self.is_blue
+    
+    def toggle_selected(self):
+        self.is_selected = not self.is_selected
         
 if __name__=="__main__":
     pygame.init()
+    ant_init_pos_x = 30
+    ant_init_pos_y = 30
+    ant_final_pos_x = 30
+    ant_final_pos_y = 30
     screen = pygame.display.set_mode((1000, 500))
     done = False
-    sim_1 = Simulator(np.array([30,30]), np.array([30,30]))
-
+    ant_1 = Ant(np.array([ant_init_pos_x, ant_init_pos_y]))
     clock = pygame.time.Clock()
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                sim_1.toggle_color()
+                ant_1.toggle_color()
             #if event.type == pygame.MOUSEMOTION:
             #    print "mouse at (%d, %d)" % event.pos
             if event.type == pygame.MOUSEBUTTONDOWN:
-                sim_1.set_destination(np.array([event.pos[0], event.pos[1]]))
+                ant_1.set_destination(np.array([event.pos[0], event.pos[1]]))
         pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP]: sim_1.increment_y_current(-3)
-        if pressed[pygame.K_DOWN]: sim_1.increment_y_current(3)
-        if pressed[pygame.K_LEFT]: sim_1.increment_x_current(-3) 
-        if pressed[pygame.K_RIGHT]: sim_1.increment_x_current(3)
+        if pressed[pygame.K_UP]: ant_1.increment_y_current_pos(-3)
+        if pressed[pygame.K_DOWN]: ant_1.increment_y_current_pos(3)
+        if pressed[pygame.K_LEFT]: ant_1.increment_x_current_pos(-3) 
+        if pressed[pygame.K_RIGHT]: ant_1.increment_x_current_pos(3)
         
-        sim_1.advance_simple()
+        ant_1.advance_simple()
         
         screen.fill((255, 255, 255))
-        if sim_1.is_blue: color = (0, 0, 0)
+        if ant_1.is_blue: color = (0, 0, 0)
         else: color = (255, 100, 0)
         ant_grid_size = 10 #must be even
-        pygame.draw.rect(screen, color, pygame.Rect(sim_1.return_x_current()-ant_grid_size/2, sim_1.return_y_current()-ant_grid_size/2, ant_grid_size, ant_grid_size))      
+        pygame.draw.rect(screen, color, pygame.Rect(ant_1.return_x_current_pos()-ant_grid_size/2, ant_1.return_y_current_pos()-ant_grid_size/2, ant_grid_size, ant_grid_size))      
         pygame.display.flip()
         clock.tick(60)
